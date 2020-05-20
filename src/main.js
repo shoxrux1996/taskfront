82a4fd11da83a5dae13c3d50b4885a36e94b2b47
shoxrux1996 <shohrux.shomaxmudov@gmail.com>
2020-05-20 17:18:43 +0500

    New installation Vue cli

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 
 Vue.config.productionTip = false
 
